f022d824a595239947001d8b963de468e2c046e3
Segun Hassan <segun.hassan@canarie.ca>
2023-03-20 19:17:43 +0000

    Add 'Workflow/ResourcePool.sh'

---
+++ Workflow/ResourcePool.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+#make the script executable
+#chmod a+x ~/launchcode.sh
+
+#install docker and all dependencies
+sudo apt-get update -y && sudo apt-get upgrade -y
+sudo apt-get install python3-pip -y
+sudo apt-get install docker-compose -y
+sudo systemctl enable --now docker
+sudo groupadd docker
+sudo usermod -aG docker $(ls /home/* -d | head -n 1 | cut -d/ -f3)
+sudo chmod 666 /var/run/docker.sock
+
+#create directory tree for boosterpack environment
+mkdir /home/$(ls /home/* -d | head -n 1 | cut -d/ -f3)/BPCode
+cd /home/$(ls /home/* -d | head -n 1 | cut -d/ -f3)/BPCode
+
+#clone repository
+git clone https://github.com/LeslieMills/GILBP.git
+cd GILBP
+
+
+#Run the application
+docker pull devashishupadhyay/scikit-learn-docker
+docker-compose up --build --detach
