65872bfb560f569c9f76dd83f5413fb06bb6b5e7
Segun Hassan <segun.hassan@canarie.ca>
2023-03-20 21:36:48 +0000

    Update 'Workflow/launchcode.sh'

--- Workflow/launchcode.sh
+++ Workflow/launchcode.sh
@@ -4,6 +4,7 @@
 #chmod a+x ~/launchcode.sh
 
 #install docker and all dependencies
+echo "Installing docker and all dependencies"
 sudo apt-get update -y && sudo apt-get upgrade -y
 sudo apt-get install python3-pip -y
 sudo apt-get install docker-compose -y
@@ -13,13 +14,16 @@ sudo usermod -aG docker $(ls /home/* -d | head -n 1 | cut -d/ -f3)
 sudo chmod 666 /var/run/docker.sock
 
 #create directory tree for boosterpack environment
+echo "Creating directory tree for boosterpack environment"
 mkdir /home/$(ls /home/* -d | head -n 1 | cut -d/ -f3)/BPCode
 cd /home/$(ls /home/* -d | head -n 1 | cut -d/ -f3)/BPCode
 
 #clone repository
+echo "Cloning repository
 git clone https://github.com/LeslieMills/GILBP.git
 cd GILBP
 
 #Run the application
+echo "Running the application"
 docker pull devashishupadhyay/scikit-learn-docker
 docker-compose up --build --detach
